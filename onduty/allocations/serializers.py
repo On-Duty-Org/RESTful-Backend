0ca6d657b7d7648737f91176e70cd9b0f689d2b6
Aman <bhatnagar.aman1998@gmail.com>
2020-07-20 13:55:52 +0530

    Getting names instead of id's for foreign keys

--- onduty/allocations/serializers.py
+++ onduty/allocations/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from . models import allocations
 
 class allocationsSerializer(serializers.ModelSerializer):
+	zone_name= serializers.ReadOnlyField(source='zone.name')        # To ensure that we get names instead of id's while taking foreign key
+	police_name= serializers.ReadOnlyField(source='police.name')
+
 
 	class Meta:
 		model = allocations
-		fields = '__all__'
+		fields = ('id', 'zone_name', 'police_name', 'date_posted', 'time', 'date')
